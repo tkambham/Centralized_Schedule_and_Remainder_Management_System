45e416b8660439c1ba2c8dc03e1b8ecd143e75f5
trilok <trilokkambham2001@gmail.com>
2024-09-03 11:56:46 -0500

    init commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB8TVDSUQMBEji2dM0-opIJFyQ1pTHo9Ng",
+    authDomain: "tkambham-cmsc5373.firebaseapp.com",
+    projectId: "tkambham-cmsc5373",
+    storageBucket: "tkambham-cmsc5373.appspot.com",
+    messagingSenderId: "129374778944",
+    appId: "1:129374778944:web:f04c7542c56e0b06e591a5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
