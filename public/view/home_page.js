09c5bcd0255e48101e2bfc5f5af4d51c888e36cd
trilok <trilokkambham2001@gmail.com>
2024-09-04 14:13:57 -0500

    update 2(mock up pages added)

---
+++ public/view/home_page.js
@@ -0,0 +1,6 @@
+import { root } from "./elements.js";
+
+export function homePageView()
+{
+    root.innerHTML = '<h1>Home Page</h1>';
+}
---
+++ public/view/menu2_page.js
@@ -0,0 +1,6 @@
+import { root } from "./elements.js";
+
+export function menu2PageView()
+{
+    root.innerHTML = '<h1>Menu 2 Page</h1>';
+}
