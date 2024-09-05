4efbc9eb2a92505caf074c708cdf15deeef39d83
trilok <trilokkambham2001@gmail.com>
2024-09-05 15:41:51 -0500

    update 5.2(Navigation Done)

--- public/app.js
+++ public/app.js
@@ -15,3 +15,10 @@ window.onload = function(e){
     // console.log(pathname, hash)
     routing(pathname, hash);
 }
+
+window.onpopstate = function(e){
+    e.preventDefault();
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    routing(pathname, hash);
+}
