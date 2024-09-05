b065c936b09dc533f3207e7733284a55ea3086d4
trilok <trilokkambham2001@gmail.com>
2024-09-05 13:25:40 -0500

    update 3.2(signin auth)

---
+++ public/controller/firebase_auth.js
@@ -0,0 +1,6 @@
+export function signinFirebase(e)
+{
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+}
--- public/view/signin_page.js
+++ public/view/signin_page.js
@@ -1,3 +1,4 @@
+import { signinFirebase } from "../controller/firebase_auth.js";
 import { root } from "./elements.js";
 
 export async function signinPageView()
@@ -10,6 +11,9 @@ export async function signinPageView()
     divWrapper.style.width = "400px";
     divWrapper.classList.add('m-4', 'p-4')
     divWrapper.innerHTML = await response.text();
+
+    const form = divWrapper.getElementsByTagName('form')[0];
+    form.onsubmit = signinFirebase;
 
     root.innerHTML = '';
     root.appendChild(divWrapper);
