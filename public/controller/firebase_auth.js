701740802f52ed5f603c81c41f26a0290535e4a8
trilok <trilokkambham2001@gmail.com>
2024-09-05 13:35:34 -0500

    update 3.3(signin libraries imported)

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,3 +1,8 @@
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js"
+import { app } from "./firebase.js";
+
+const auth = getAuth(app);
+
 export function signinFirebase(e)
 {
     e.preventDefault();
