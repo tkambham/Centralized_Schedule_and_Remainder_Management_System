02961009b97810aa78ac7c177d9da3d1f1ef6832
trilok <trilokkambham2001@gmail.com>
2024-09-05 15:38:11 -0500

    update 5.1(Navigation part 1)

--- public/controller/menueventhandlers.js
+++ public/controller/menueventhandlers.js
@@ -1,12 +1,15 @@
 import { homePageView } from '../view/home_page.js';
 import { menu2PageView } from '../view/menu2_page.js';
 import { signOutFirbase } from './firebase_auth.js';
+import { routePathnames } from './route_controller.js';
 
 export function onClickHomeMenu(e) {
+    history.pushState(null, null, routePathnames.HOME);
     homePageView();
 }
 
 export function onClickMenu2Menu(e) {
+    history.pushState(null, null, routePathnames.MENU2);
     menu2PageView();
 }
 
