3a695d2911397438140bd5a2c5f544e2822e8ad8
trilok <trilokkambham2001@gmail.com>
2024-11-12 15:10:51 -0600

    update 10.1(web to firestore saving code updated)

--- public/controller/schedule_controller.js
+++ public/controller/schedule_controller.js
@@ -1,10 +1,13 @@
+import { Appointments } from "../model/appointments.js";
+import { currentUser } from "./firebase_auth.js";
+
 export function onChangeApponinmentTime(e) {
     const time = e.target.value;
     const earlyRemainder = document.querySelector('#early-remainder');
     earlyRemainder.classList.replace('d-none', 'd-block');
 }
 
-export function onClickSaveAppointment(e) {
+export async function onClickSaveAppointment(e) {
     e.preventDefault();
     const appointmentTitle = e.target.appointment_title.value.trim();
     const appointmentDate = e.target.appointment_date.value;
@@ -14,4 +17,26 @@ export function onClickSaveAppointment(e) {
     const appointmentNotes = e.target.appointment_notes.value.trim();
 
     console.log(appointmentTitle, appointmentDate, appointmentTime, earlyRemainder, appointmentType, appointmentNotes);
+
+    const appointment = new Appointments({
+        appointmentTitle,
+        appointmentDate,
+        appointmentTime,
+        earlyRemainder,
+        appointmentType,
+        appointmentNotes,
+        createdBy: currentUser.email
+    });
+
+    try{
+        const docId = await addAppointment(appointment);
+        Appointments.set_docId(docId);
+    }
+    catch(e){
+        console.log('Failed to save appointment', e);
+        alert('Failed to save appointment' + JSON.stringify(e));
+        return;
+    }
+
+    e.target.reset();
 }
