918aaf83f464c05a7d1daef0fc250b0ce146530a
trilok <trilokkambham2001@gmail.com>
2024-11-12 12:06:00 -0600

    update 8.4(Schedule appointemnet card created)

--- public/view/schedule_page.js
+++ public/view/schedule_page.js
@@ -2,11 +2,18 @@ import { currentUser } from "../controller/firebase_auth.js";
 import { root } from "./elements.js";
 import { protectedView } from "./protected_view.js";
 
-export async function schedulePageView()
-{
-    if(!currentUser){
+export async function schedulePageView() {
+    if (!currentUser) {
         root.innerHTML = await protectedView();
         return;
     }
-    root.innerHTML = '<h1>Menu2 Page</h1>';
+    const response = await fetch('/view/templates/schedule_page_template.html',
+        { cache: "no-store" });
+
+    const divWrapper = document.createElement('div');
+    divWrapper.innerHTML = await response.text();
+    divWrapper.classList.add('m-4', 'p-4');
+
+    root.innerHTML = '';
+    root.appendChild(divWrapper);
 }
