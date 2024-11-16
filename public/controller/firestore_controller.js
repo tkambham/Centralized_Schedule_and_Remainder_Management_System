b956510b2f31eb5fa6801dfbd2075dcff0f7193f
trilok <trilokkambham2001@gmail.com>
2024-11-16 17:09:24 -0600

    update 14.2.1(unwanted code removed)

--- public/controller/firestore_controller.js
+++ public/controller/firestore_controller.js
@@ -12,8 +12,6 @@ import {
  } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
 
 import { app } from "./firebase_core.js";
-import { currentUser } from "./firebase_auth.js";
-import { DEV } from "../model/constants.js";
 import { Appointments } from "../model/appointments.js";
 
 const APPOINTMENT_COLLECTION = 'appointments_collection';
